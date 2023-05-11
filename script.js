d9feeddeda28dc713aef00a8409136ac28b558aa
Peter Conenna <pconenna@depaul.edu>
2023-05-11 13:36:42 -0500

    added icons to weather cards and deleted unsued css file

--- script.js
+++ script.js
@@ -3,18 +3,20 @@ var btnSearch = document.querySelector('#btnSearch');
 var searchInput = document.querySelector('#searchInput');
 var currentInfo = document.querySelector('#currentInfo');
 var searchHistory = document.querySelector('#searchHistory');
-var historyButtons = document.querySelectorAll('.btn-secondary'); // this is a list of every button in the history
+var iconEl = document.querySelector('#icon');
 var daysDiv = document.querySelector('#daysDiv');
+var storedHistory = JSON.parse(localStorage.getItem("searches"))
+if(storedHistory == null){
+    storedHistory = [];
+    localStorage.setItem("searches", JSON.stringify(storedHistory))
+}
 // var day1 = document.querySelector('#day1');
 // var day2 = document.querySelector('#day2');
 // var day3 = document.querySelector('#day3');
 // var day4 = document.querySelector('#day4');
 // var day5 = document.querySelector('#day5');
 var lat,lon;
-
-
-// i need: search button, the div the history goes in, the buttons that go in there, the cards
-// emojis reperesenting weather? low priority
+getStoredHistory()
 function getCoords(event){
     event.preventDefault();
     var searchTerm = searchInput.value.trim();
@@ -27,7 +29,30 @@ function getCoords(event){
     .then(function(response){
         if(response.ok){
             response.json().then(function(data){
-                
+                console.log(data)
+                lon = data[0].lon;
+                lat = data[0].lat;
+                getCurrentWeather(lon,lat)
+                getForceast(lon,lat)
+                addSearchHistory(data[0].name)
+
+            })
+        }
+    })
+}
+
+// i had to put some partially redundant code because i need event listeners for the search from and the history buttons
+function getCoordsHistory(event){
+    var searchTerm = event.target.textContent.trim();
+    var geoCodeURL = `http://api.openweathermap.org/geo/1.0/direct?q=${searchTerm},US&limit=1&appid=1dfd6ca54fd859622c9fe8699a7944f0`
+    if(!searchTerm){
+        console.log("bad input")
+        return;
+    }
+    fetch(geoCodeURL)
+    .then(function(response){
+        if(response.ok){
+            response.json().then(function(data){
                 lon = data[0].lon;
                 lat = data[0].lat;
                 getCurrentWeather(lon,lat)
@@ -37,6 +62,7 @@ function getCoords(event){
         }
     })
 }
+
 function getCurrentWeather(lon, lat){
     var weatherURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=1dfd6ca54fd859622c9fe8699a7944f0`;
     if(!lon || !lat){
@@ -55,8 +81,12 @@ function getCurrentWeather(lon, lat){
 
 function showCurrentWeather(data){
     var cityName = document.querySelector("#cityName");
-    console.log(data.weather.icon) //figure out the icon
-    cityName.textContent = `${data.name}`;
+    var today = dayjs();
+    var icon = data.weather[0].icon
+    console.log(data.weather[0].icon) //figure out the icon
+    var imgUrl = `https://openweathermap.org/img/wn/${icon}.png`
+    iconEl.setAttribute("src",imgUrl)
+    cityName.textContent = `${data.name} ${today.format('M-D-YYYY')}`;
     var currentTemp = document.querySelector('#currentTemp');
     var currentWind = document.querySelector('#currentWind');
     var currentHumidity = document.querySelector('#currentHumidity');
@@ -75,7 +105,7 @@ function getForceast(){
     .then(function(response){
         if(response.ok){
             response.json().then(function(data){
-                console.log(data);
+                // console.log(data);
                 showForecast(data)
             })
         }
@@ -83,6 +113,8 @@ function getForceast(){
 }
 function showForecast(data){
     var days = data.list;
+    console.log(data)
+    
     // 0,8,16,24,32 are midnight of new days
     // 4,12,20,28,36 are noon
     var dayCards = daysDiv.children 
@@ -90,9 +122,13 @@ function showForecast(data){
     for( var d = 0; d < days.length; d++ ){
         if((d == 0 || d%4 == 0) && d%8!=0){ //the data shown is based off the array index representing noon
             var nextDay = days[d].dt_txt
+            var icon = days[d].weather[0].icon
             var t = dayjs(nextDay)
             var dayTitle = dayCards[cardNum].children[0].children[0]
-            var tempEl = dayTitle.nextElementSibling
+            var iconE = dayTitle.nextElementSibling
+            var imgUrl = `https://openweathermap.org/img/wn/${icon}.png`
+            iconE.setAttribute("src",imgUrl)
+            var tempEl = iconE.nextElementSibling
             var windEl = tempEl.nextElementSibling
             var humidEl = windEl.nextElementSibling
             dayTitle.textContent = t.format('M-D-YYYY');
@@ -109,9 +145,29 @@ function showForecast(data){
    
     }
 
+function addSearchHistory(city){
+    var btn = document.createElement("button")
+    btn.textContent = city;
+    btn.setAttribute("class", "col-9 btn btn-secondary m-3")
+    btn.addEventListener("click",getCoordsHistory)
+    searchHistory.appendChild(btn)
+    storedHistory.push({'searchTerm':city})
+    localStorage.setItem("searches", JSON.stringify(storedHistory))
+}
+
+function getStoredHistory(){
+    for(var i = 0; i < storedHistory.length; i++){
+        var btn = document.createElement("button")
+        btn.textContent = storedHistory[i].searchTerm;
+        btn.setAttribute("class", "col-9 btn btn-secondary m-3")
+        btn.addEventListener("click",getCoordsHistory)
+        searchHistory.appendChild(btn)
+    }
+    
+}
 searchform.addEventListener("submit", getCoords)
-var btnH = document.querySelector('#btnHistory')
-btnH.addEventListener("click",getCoords)
+// var btnH = document.querySelector('#btnHistory')
+// btnH.addEventListener("click",getCoordsHistory)
 
 //event handler for every button
 
